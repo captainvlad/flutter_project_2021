0586b6917d6c49068a5dbb710d32d34181f3b487
captainvlad <zadorozhnyy@ucu.edu.ua>
2022-01-30 23:17:55 +0200

    Fixed some additional moments (widgets, icons, app name)

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -28,9 +28,49 @@ class LoadingScreen extends StatelessWidget {
           SizedBox(
             height: uiManager.blockSizeVertical * 20,
           ),
-          CircularProgressIndicator(
-            color: yellowColor,
-            strokeWidth: uiManager.blockSizeVertical * 1,
+          Stack(
+            children: [
+              SizedBox(
+                width: uiManager.blockSizeHorizontal * 15,
+                height: uiManager.blockSizeHorizontal * 15,
+                child: CircularProgressIndicator(
+                  color: whiteColor,
+                  strokeWidth: uiManager.blockSizeVertical / 2,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                  uiManager.blockSizeHorizontal * 1,
+                  uiManager.blockSizeHorizontal * 1,
+                  0.0,
+                  0.0,
+                ),
+                child: SizedBox(
+                  width: uiManager.blockSizeHorizontal * 13,
+                  height: uiManager.blockSizeHorizontal * 13,
+                  child: CircularProgressIndicator(
+                    color: yellowColor,
+                    strokeWidth: uiManager.blockSizeVertical,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                  uiManager.blockSizeHorizontal * 2,
+                  uiManager.blockSizeHorizontal * 2,
+                  0.0,
+                  0.0,
+                ),
+                child: SizedBox(
+                  width: uiManager.blockSizeHorizontal * 11,
+                  height: uiManager.blockSizeHorizontal * 11,
+                  child: CircularProgressIndicator(
+                    color: whiteColor,
+                    strokeWidth: uiManager.blockSizeVertical / 2,
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
